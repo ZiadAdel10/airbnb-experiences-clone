dd70a7b3c0750ff1007eeaf6c3ed1d5f52fbf267
ZiadAdel10 <ziad.adel.mahmoud@gmail.com>
2023-03-20 02:10:09 +0200

    project files

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
+import data from "./data";
+
+export default function App() {
+  const cards = data.map((item) => {
+    return <Card key={item.id} {...item} />;
+  });
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="main--section">
+        <Hero />
+        <section className="cards-list">{cards}</section>
+      </div>
+    </div>
+  );
+}
